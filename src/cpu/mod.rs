3b326012ba1a914c3efab1ab2a10736b83d30870
Asami Doi <doiasami1219@gmail.com>
2019-11-03 22:44:48 +0900

    Simplify code struct

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -13,102 +13,15 @@ pub struct Cpu {
     pub pc: usize,
 }
 
-// TODO: consider to divide into multiple `type` structs
-/*
-#[derive(Debug)]
-struct RType {
-    opcode: u8,
-    funct3: u8,
-    funct7: u8,
-    rd: u8,
-    rs1: u8,
-    rs2: u8,
-}
-
-#[derive(Debug)]
-struct IType {
-    opcode: u8,
-    funct3: u8,
-    rd: u8,
-    rs1: u8,
-    imm: u32,
-}
-
-#[derive(Debug)]
-enum Type {
-    R_type(RType),
-    I_type(IType),
-    Unknown,
-}
-
 #[derive(Debug)]
 pub struct Code {
-    ty: Type
-}
-*/
-
-#[derive(Debug)]
-pub struct Code {
-    opcode: u8,
-    funct3: u8,
-    funct7: u8,
-    rd: u8,
-    rs1: u8,
-    rs2: u8,
+    opcode: u32,
+    funct3: u32,
+    funct7: u32,
+    rd: u32,
+    rs1: u32,
+    rs2: u32,
     imm: u32,
-}
-
-impl Code {
-    fn new(binary: u32) -> Code {
-        let opcode: u8 = (binary & 0x0000007F).try_into().unwrap();
-
-        match opcode {
-            0x13 => { // I type
-                let rd: u8 = ((binary & 0x00000F80).rotate_right(7))
-                    .try_into().unwrap();
-                let rs1 : u8 = ((binary & 0x000F8000).rotate_right(15))
-                    .try_into().unwrap();
-                let imm : u32 = (binary & 0xFFF00000).rotate_right(20);
-                return Code {
-                    opcode,
-                    funct3: 0,
-                    funct7: 0,
-                    rd,
-                    rs1,
-                    rs2: 0,
-                    imm,
-                };
-            }
-            0x33 => { // R type
-                let rd: u8 = ((binary & 0x00000F80).rotate_right(7))
-                    .try_into().unwrap();
-                let rs1 : u8 = ((binary & 0x000F8000).rotate_right(15))
-                    .try_into().unwrap();
-                let rs2 : u8 = ((binary & 0x01F00000).rotate_right(20))
-                    .try_into().unwrap();
-                return Code {
-                    opcode,
-                    funct3: 0,
-                    funct7: 0,
-                    rd,
-                    rs1,
-                    rs2,
-                    imm: 0,
-                };
-            }
-            _ => {}
-        }
-
-        Code {
-            opcode: opcode,
-            funct3: 0,
-            funct7: 0,
-            rd: 0,
-            rs1: 0,
-            rs2: 0,
-            imm: 0,
-        }
-    }
 }
 
 impl Cpu {
@@ -136,7 +49,14 @@ impl Cpu {
     }
 
     pub fn decode(&self, binary: u32) -> Code {
-        Code::new(binary)
+        let opcode = binary & 0x0000007F;
+        let rd = (binary & 0x00000F80) >> 7;
+        let funct3 = (binary & 0x00007000) >> 12;
+        let rs1 = (binary & 0x000F8000) >> 15;
+        let rs2 = (binary & 0x01F00000) >> 20;
+        let imm = (binary & 0xFFF00000) >> 20;
+        let funct7 = (binary & 0xFE000000) >> 25;
+        Code{ opcode, funct3, funct7, rd, rs1, rs2, imm }
     }
 
     pub fn execute(&mut self, code: &Code, memory: &mut Vec<u8>) {
