9f9d0861a2ab62f26cb52bfae1182b5350736c87
Asami Doi <doiasami1219@gmail.com>
2019-11-04 11:29:09 +0900

    Delete the instruction file

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,34 +1,15 @@
-pub mod instruction;
-
 use crate::*;
-use crate::cpu::instruction::*;
 
 pub const REGISTERS_COUNT: usize = 32;
 
 pub struct Cpu {
-    pub instructions: [Option<InstFunc>; 127],
     pub registers: [u32; REGISTERS_COUNT],
     pub pc: usize,
 }
 
-#[derive(Debug)]
-pub struct Code {
-    opcode: u32,
-    funct3: u32,
-    funct7: u32,
-    rd: u32,
-    rs1: u32,
-    rs2: u32,
-    imm: u32,
-}
-
 impl Cpu {
     pub fn new() -> Cpu {
-        let mut insts = [None; 127];
-        init_instructions(&mut insts);
-
         Cpu {
-            instructions: insts,
             registers: [0; REGISTERS_COUNT],
             pc: 0,
         }
@@ -39,8 +20,7 @@ impl Cpu {
 
         while self.pc < size {
             let binary = self.fetch(memory);
-            let code = self.decode(binary);
-            self.execute(&code, memory);
+            self.execute(binary, memory);
         }
     }
 
@@ -56,29 +36,32 @@ impl Cpu {
         return bin;
     }
 
-    fn decode(&self, binary: u32) -> Code {
+    fn execute(&mut self, binary: u32, memory: &mut Vec<u8>) {
         let opcode = binary & 0x0000007F;
-        let rd = (binary & 0x00000F80) >> 7;
+        let rd = ((binary & 0x00000F80) >> 7) as usize;
+        let rs1 = ((binary & 0x000F8000) >> 15) as usize;
+        let rs2 = ((binary & 0x01F00000) >> 20) as usize;
         let funct3 = (binary & 0x00007000) >> 12;
-        let rs1 = (binary & 0x000F8000) >> 15;
-        let rs2 = (binary & 0x01F00000) >> 20;
-        let imm = (binary & 0xFFF00000) >> 20;
         let funct7 = (binary & 0xFE000000) >> 25;
-        Code{ opcode, funct3, funct7, rd, rs1, rs2, imm }
-    }
+        let imm = (binary & 0xFFF00000) >> 20;
 
-    fn execute(&mut self, code: &Code, memory: &mut Vec<u8>) {
-        let opcode: usize = code.opcode as usize;
-
-        let text = format!("pc = {:x}, opcode = {:#x} ({}, {:#b}), code = {:#?})",
-            self.pc, opcode, opcode, opcode, code);
+        let text = format!("pc = {:x}, opcode = {:#x} ({}, {:#b})",
+            self.pc, opcode, opcode, opcode);
         log(&text);
         render(&text);
 
-        match self.instructions[opcode] {
-            Some(inst) => inst(self, &code, memory),
-            None => {
-                let text = format!("not implemented: opecode {}, {:#?}", opcode, code);
+        match opcode {
+            0x13 => {
+                // addi rd, rs1, imm (0x13): I-type. rd = rs1 + imm
+                self.registers[rd] = self.registers[rs1] + imm;
+            },
+            0x33 => {
+                // add rd, rs1, rs2 (0x33): R-type. rd = rs1 + rs2
+                self.registers[rd] = self.registers[rs1] + self.registers[rs2];
+            },
+            _ => {
+                let text = format!("not implemented: opecode {:#x} ({}, {:#b})",
+                    opcode, opcode, opcode);
                 log(&text);
                 render(&text);
                 return;
