6b768c506619792b81bcab988231be1c2fc64f11
Asami Doi <doiasami1219@gmail.com>
2019-11-13 15:59:29 +0900

    Add tests for shift instructions in RV64I

--- tests/rv64i.rs
+++ tests/rv64i.rs
@@ -31,7 +31,7 @@ pub fn slliw_rd_rs1_imm() {
     let mut mem = vec![
         // addiw x31, x0, 4
         0x9B, 0x0F, 0x40, 0x00,
-        // slliw x32, x31, 3
+        // slliw x30, x31, 3
         0x1B, 0x9F, 0x3F, 0x00,
     ];
 
@@ -51,7 +51,7 @@ pub fn srliw_rd_rs1_imm() {
     let mut mem = vec![
         // addiw x31, x0, 4
         0x9B, 0x0F, 0x40, 0x00,
-        // srliw x32, x31, 1
+        // srliw x30, x31, 1
         0x1B, 0xDF, 0x1F, 0x00,
     ];
 
@@ -71,7 +71,7 @@ pub fn sraiw_rd_rs1_imm() {
     let mut mem = vec![
         // addiw x31, x0, -4
         0x9B, 0x0F, 0xCF, 0xFF,
-        // sraiw x32, x31, 1
+        // sraiw x30, x31, 1
         0x1B, 0xDF, 0x1F, 0x40,
     ];
 
@@ -80,6 +80,116 @@ pub fn sraiw_rd_rs1_imm() {
     let expected =
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, -4];
+    for (i, e) in expected.iter().enumerate() {
+        assert_eq!(*e, cpu.regs[i]);
+    }
+}
+
+#[wasm_bindgen_test]
+pub fn addw_rd_rs1_rs2() {
+    let mut cpu = rvemu::cpu::Cpu::new();
+    let mut mem = vec![
+        // addiw x31, x0, -4
+        0x9B, 0x0F, 0xCF, 0xFF,
+        // addiw x30, x0, 8
+        0x1B, 0x0F, 0x80, 0x00,
+        // addw x29, x30, x31
+        0xBB, 0x0E, 0xFF, 0x01,
+    ];
+
+    cpu.start(&mut mem);
+
+    let expected =
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 8, -4];
+    for (i, e) in expected.iter().enumerate() {
+        assert_eq!(*e, cpu.regs[i]);
+    }
+}
+
+#[wasm_bindgen_test]
+pub fn subw_rd_rs1_rs2() {
+    let mut cpu = rvemu::cpu::Cpu::new();
+    let mut mem = vec![
+        // addiw x31, x0, -4
+        0x9B, 0x0F, 0xCF, 0xFF,
+        // addiw x30, x0, 8
+        0x1B, 0x0F, 0x80, 0x00,
+        // subw x29, x30, x31
+        0xBB, 0x0E, 0xFF, 0x41,
+    ];
+
+    cpu.start(&mut mem);
+
+    let expected =
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 8, -4];
+    for (i, e) in expected.iter().enumerate() {
+        assert_eq!(*e, cpu.regs[i]);
+    }
+}
+
+#[wasm_bindgen_test]
+pub fn sllw_rd_rs1_rs2() {
+    let mut cpu = rvemu::cpu::Cpu::new();
+    let mut mem = vec![
+        // addiw x31, x0, 2
+        0x9B, 0x0F, 0x20, 0x00,
+        // addiw x30, x0, 8
+        0x1B, 0x0F, 0x80, 0x00,
+        // sllw x29, x30, x31
+        0xBB, 0x1E, 0xFF, 0x01,
+    ];
+
+    cpu.start(&mut mem);
+
+    let expected =
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 8, 2];
+    for (i, e) in expected.iter().enumerate() {
+        assert_eq!(*e, cpu.regs[i]);
+    }
+}
+
+#[wasm_bindgen_test]
+pub fn srlw_rd_rs1_rs2() {
+    let mut cpu = rvemu::cpu::Cpu::new();
+    let mut mem = vec![
+        // addiw x31, x0, 2
+        0x9B, 0x0F, 0x20, 0x00,
+        // addiw x30, x0, 8
+        0x1B, 0x0F, 0x80, 0x00,
+        // srlw x29, x30, x31
+        0xBB, 0x5E, 0xFF, 0x01,
+    ];
+
+    cpu.start(&mut mem);
+
+    let expected =
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 8, 2];
+    for (i, e) in expected.iter().enumerate() {
+        assert_eq!(*e, cpu.regs[i]);
+    }
+}
+
+#[wasm_bindgen_test]
+pub fn sraw_rd_rs1_rs2() {
+    let mut cpu = rvemu::cpu::Cpu::new();
+    let mut mem = vec![
+        // addiw x31, x0, 2
+        0x9B, 0x0F, 0x20, 0x00,
+        // addiw x30, x0, -8
+        0x1B, 0x0F, 0x8F, 0xFF,
+        // sraw x29, x30, x31
+        0xBB, 0x5E, 0xFF, 0x41,
+    ];
+
+    cpu.start(&mut mem);
+
+    let expected =
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, -8, 2];
     for (i, e) in expected.iter().enumerate() {
         assert_eq!(*e, cpu.regs[i]);
     }
