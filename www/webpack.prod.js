7bb7bd9e97e3061be33b3f6c36387fec0d55f921
Asami Doi <doiasami1219@gmail.com>
2020-01-12 15:40:12 +0900

    Delete www directory

--- www/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const nodeExternals = require('webpack-node-externals');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  mode: "production",
-  externals: [nodeExternals()],
-});
